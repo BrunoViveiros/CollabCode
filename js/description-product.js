00e6e2f71ae568e1698070341c38600a47565ced
Ray Zyphs <bruferviveiros@gmail.com>
2019-08-15 14:31:47 -0300

    Desafio Básico - Aula 014

--- js/description-product.js
+++ js/description-product.js
@@ -3,5 +3,11 @@ const $heart = window.document.querySelector(".-heart");
 $heart.addEventListener("click", handleClick)
 
 function handleClick() {
-  $heart.classList.toggle('-active');
+  if ($heart.classList.contains('-ativo')) {
+    $heart.firstChild.setAttribute('src', '../assets/heart.png')
+    $heart.classList.remove('-ativo')
+  } else {
+    $heart.firstChild.setAttribute('src', '../assets/heart-active.png')
+    $heart.classList.add('-ativo')
+  }
 }
