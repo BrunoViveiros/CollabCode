e056fe62cc89482602f534fa110b6e5a98e87719
Ray Zyphs <bruferviveiros@gmail.com>
2019-08-15 15:03:48 -0300

    Desafio Intermediário, Avançado - Aula 014

--- js/description-product.js
+++ js/description-product.js
@@ -11,3 +11,34 @@ function handleClick() {
     $heart.classList.add('-ativo')
   }
 }
+
+
+const $stars = document.querySelectorAll(".-star .icon");
+
+$stars.forEach(function(star, index){
+  star.addEventListener("click", function() {
+    handleStar(index);
+  }) 
+})    
+
+function handleStar(index) {
+
+  if ($stars[index].classList.contains('-active')) {
+    for (let i = 4; i > index; i--) {
+      $stars[i].classList.remove('-active');
+      $stars[i].setAttribute('src', '../assets/star.png')
+    }
+
+  } else {
+    for (let i = 0; i <= index; i++) {
+      $stars[i].classList.add('-active');
+      $stars[i].setAttribute('src', '../assets/star-full.png')
+    }
+    
+  }
+  
+}
+
+
+
+
