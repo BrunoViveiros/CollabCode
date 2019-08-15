612f5ce6e5fb4bc690252d9841ed00b34e5954aa
Ray Zyphs <bruferviveiros@gmail.com>
2019-08-15 10:29:57 -0300

    Reset Desafio - Aula 013

--- js/description-product.js
+++ js/description-product.js
@@ -1,19 +1,15 @@
+/*
+1 - Ter a janela (browser) na mão => OK
+2 - Ter o HTML na mão => OK
+3 - Pegar o coração => OK
+4 - Pegar clique no  coração
+5 - No momento que o usuário clicar, nós queremos mostrar um texto "AE"
+*/
+
 const $heart = window.document.querySelector(".-heart");
-const $button = window.document.querySelector("button.-second");
-const $carrinho = window.document.querySelector(".navigation > .-last");
-let items = 10;
 
 $heart.addEventListener("click", handleClick)
-$button.addEventListener("click", addItemCarrinho)
 
 function handleClick() {
   console.log('ae');
-}
-
-function addItemCarrinho() {
-  handleClick();
-  
-  items ++;
-
-  $carrinho.innerText = `Carrinho (${items})`;
 }
