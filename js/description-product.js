b872995ac22c587f4039bbb0bb12cbe0ca176ba0
Ray Zyphs <bruferviveiros@gmail.com>
2019-08-16 15:44:21 -0300

    Desafio Avançado - Aula 015

--- js/description-product.js
+++ js/description-product.js
@@ -3,11 +3,27 @@ const $stars = document.querySelectorAll(".star");
 
 $heart.addEventListener("click", handleClick)
 
-$stars.forEach(function ($star) {
-  $star.addEventListener('click', handleClick)
+$stars.forEach(function ($star, index) {
+  $star.addEventListener('click', () => {
+    handleStar($star, index);
+  })
 })
-
 
 function handleClick() {
   this.classList.toggle('-active')
+}
+
+function handleStar($star, index) {
+  const lastKey = $stars.length - 1;
+
+  if ($star.classList.contains('-active')) {
+    for (let i = lastKey; i > index; i--) {
+      $stars[i].classList.remove('-active');
+    }
+
+  } else {
+    for (let i = 0; i <= index; i++) {
+      $stars[i].classList.add('-active');
+    }
+  }
 }
