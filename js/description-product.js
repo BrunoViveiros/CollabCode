9dcfd4e1b98d4fed09ada9960000985861dad8fd
Ray Zyphs <bruferviveiros@gmail.com>
2019-08-19 10:43:04 -0300

    Reset Desafio - Aula 015

--- js/description-product.js
+++ js/description-product.js
@@ -3,27 +3,10 @@ const $stars = document.querySelectorAll(".star");
 
 $heart.addEventListener("click", handleClick)
 
-$stars.forEach(function ($star, index) {
-  $star.addEventListener('click', () => {
-    handleStar($star, index);
-  })
+$stars.forEach(function ($star) {
+  $star.addEventListener('click', handleClick)
 })
 
 function handleClick() {
   this.classList.toggle('-active')
-}
-
-function handleStar($star, index) {
-  const lastKey = $stars.length - 1;
-
-  if ($star.classList.contains('-active')) {
-    for (let i = lastKey; i > index; i--) {
-      $stars[i].classList.remove('-active');
-    }
-
-  } else {
-    for (let i = 0; i <= index; i++) {
-      $stars[i].classList.add('-active');
-    }
-  }
 }
