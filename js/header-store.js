65591d4a95f21056badeffc845fe72d4ee68efc9
Ray Zyphs <bruferviveiros@gmail.com>
2019-08-20 08:51:05 -0300

    Desafio - Aula 017

--- js/header-store.js
+++ js/header-store.js
@@ -1,3 +1,20 @@
 const $search = document.querySelector('[type=search]');
+const $lupa = document.querySelectorAll('.navigation .icon')[0]
+const $close = document.querySelectorAll('.navigation .icon')[1]
 
-$search.addEventListener('click', () => $search.classList.toggle("-active"));
+$lupa.addEventListener('click', () => {
+  $search.classList.toggle("-active");
+  $lupa.classList.toggle("-active");
+  $lupa.classList.toggle("-slide");
+  $close.classList.toggle("-active");
+  $close.classList.toggle("-slide");
+});
+
+$close.addEventListener('click', () => {
+  $search.classList.toggle("-active");
+  $lupa.classList.toggle("-active");
+  $lupa.classList.toggle("-slide");
+  $close.classList.toggle("-slide");
+  $close.classList.toggle("-active");
+  
+});
