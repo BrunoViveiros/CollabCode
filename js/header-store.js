e80a4a75079ccb564b9d165451c37a92948d8fb1
Ray Zyphs <bruferviveiros@gmail.com>
2019-08-21 09:26:08 -0300

    Reset Desafio - Aula 017

--- js/header-store.js
+++ js/header-store.js
@@ -1,20 +1,3 @@
 const $search = document.querySelector('[type=search]');
-const $lupa = document.querySelectorAll('.navigation .icon')[0]
-const $close = document.querySelectorAll('.navigation .icon')[1]
 
-$lupa.addEventListener('click', () => {
-  $search.classList.toggle("-active");
-  $lupa.classList.toggle("-active");
-  $lupa.classList.toggle("-slide");
-  $close.classList.toggle("-active");
-  $close.classList.toggle("-slide");
-});
-
-$close.addEventListener('click', () => {
-  $search.classList.toggle("-active");
-  $lupa.classList.toggle("-active");
-  $lupa.classList.toggle("-slide");
-  $close.classList.toggle("-slide");
-  $close.classList.toggle("-active");
-  
-});
+$search.addEventListener('click', () => $search.classList.toggle("-active"));
