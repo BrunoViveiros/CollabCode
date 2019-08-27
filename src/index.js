b0b15c9f0956afb960e356e8cbb02ed3e29780ab
Ray Zyphs <bruferviveiros@gmail.com>
2019-08-27 11:34:11 -0300

    Desafio - Aula 022

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const $root = document.querySelector("#root");
 const $memoryCard = document.createElement("article");
-const $icon = `
+const $memoryCardFront = document.createElement("article"); 
+
+const $iconCollab = `
   <img
     src='img/icon-collabcode.png'
     alt='Gueio mascote da CollabCode'
@@ -8,7 +10,23 @@ const $icon = `
   />
 `;
 
-$memoryCard.classList.add("memory-card");
+const $iconJS = `
+  <img 
+    src="img/-icon-js.png"
+    alt="Icone de um livro da linguagem JS"
+    class="icon" 
+  />
+`
 
+$memoryCard.classList.add("memory-card");
 $root.insertBefore($memoryCard, null);
-$memoryCard.insertAdjacentHTML("afterbegin", $icon);
+
+$memoryCardFront.classList.add("memory-card");
+$memoryCardFront.classList.add("-front");
+$root.insertBefore($memoryCardFront, null);
+
+
+
+$memoryCard.insertAdjacentHTML("afterbegin", $iconCollab);
+
+$memoryCardFront.insertAdjacentHTML("afterbegin", $iconJS);
