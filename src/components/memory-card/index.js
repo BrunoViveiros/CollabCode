cf49c5622d7dbc3204e70e647245c17e921b2015
Ray Zyphs <bruferviveiros@gmail.com>
2019-09-02 14:36:57 -0300

    Desafio 026

--- src/components/memory-card/index.js
+++ src/components/memory-card/index.js
@@ -10,3 +10,48 @@ const createMemoryCard = ({ src, alt, nameClass }) => `
 `;
 
 const handleClick = () => console.log('ae');
+
+window.onload = () => {
+  const $head = document.querySelector("head");
+  const $styles = document.createElement("style");
+  $styles.textContent = `
+  .memory-card {
+    width: 155px;
+    height: 155px;
+    background-color: #f25a70;
+    border-radius: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16);
+    position: relative;
+    cursor: pointer;
+  }
+  
+  .memory-card.-front {
+    background-color: #FFF;
+  }
+  
+  .memory-card.-front::before {
+    content: '';
+    width: 95px;
+    height: 95px;
+    background-color: #D4D4D4;
+    border-radius: 50%;
+    position: absolute;
+  }
+  
+  .memory-card > .icon {
+    width: 100px;
+    height: 100px;
+  }
+  
+  .memory-card.-front > .icon {
+    position: absolute;
+    transform: translateY(-10px);
+  }
+  
+  `;
+  $head.insertBefore($styles, null);
+  
+}
