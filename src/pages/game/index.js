6c8de8c0e350c0d5d8868d157170fbd6882f8fda
Ray Zyphs <bruferviveiros@gmail.com>
2019-09-11 20:53:05 -0300

    Aula 31

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -1,36 +1,38 @@
-const $root = document.querySelector('#root');
+(function () {
+  const $root = document.querySelector('#root');
 
-const $cardsWrapper = createCardsWrapper();
+  const $cardsWrapper = createCardsWrapper();
 
-const createMemoryCard = memoryCard()
+  const createMemoryCard = memoryCard()
 
-const $memoryCardC = createMemoryCard({
-  src: "img/icon-c.png",
-  alt: "Icone de um livro da linguagem C++"
-});
+  const $memoryCardC = createMemoryCard({
+    src: "img/icon-c.png",
+    alt: "Icone de um livro da linguagem C++"
+  });
 
-const $memoryCardJS = createMemoryCard({
-  src: "img/icon-js.png",
-  alt: "Icone de um livro da linguagem JS"
-});
+  const $memoryCardJS = createMemoryCard({
+    src: "img/icon-js.png",
+    alt: "Icone de um livro da linguagem JS"
+  });
 
-const $memoryCardJava = createMemoryCard({
-  src: "img/icon-java.png",
-  alt: "Icone de um livro da linguagem Java"
-});
+  const $memoryCardJava = createMemoryCard({
+    src: "img/icon-java.png",
+    alt: "Icone de um livro da linguagem Java"
+  });
 
-const $memoryCardWoman = createMemoryCard({
-  src: "img/icon-woman.png",
-  alt: "Icone de uma mulher codando"
-});
- 
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJava);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJava);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardWoman);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardWoman);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
-$cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
+  const $memoryCardWoman = createMemoryCard({
+    src: "img/icon-woman.png",
+    alt: "Icone de uma mulher codando"
+  });
+  
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJS);
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJava);
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardJava);
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardWoman);
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardWoman);
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
+  $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
 
-$root.insertAdjacentElement("beforeend", $cardsWrapper);
+  $root.insertAdjacentElement("beforeend", $cardsWrapper);
+})();
