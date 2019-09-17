5618efae5434b842c2c62cd9aabbe80bc64d6280
Ray Zyphs <bruferviveiros@gmail.com>
2019-09-17 12:58:00 -0300

    Aula 33

--- src/pages/game/index.js
+++ src/pages/game/index.js
@@ -2,24 +2,21 @@
   const $root = document.querySelector('#root');
 
   const $cardsWrapper = createCardsWrapper();
-
   const createMemoryCard = memoryCard.create();
-
+  const $pointBar = pointBar.create();
+ 
   const $memoryCardC = createMemoryCard({
     src: "img/icon-c.png",
     alt: "Icone de um livro da linguagem C++"
   });
-
   const $memoryCardJS = createMemoryCard({
     src: "img/icon-js.png",
     alt: "Icone de um livro da linguagem JS"
   });
-
   const $memoryCardJava = createMemoryCard({
     src: "img/icon-java.png",
     alt: "Icone de um livro da linguagem Java"
   });
-
   const $memoryCardWoman = createMemoryCard({
     src: "img/icon-woman.png",
     alt: "Icone de uma mulher codando"
@@ -34,5 +31,7 @@
   $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
   $cardsWrapper.insertAdjacentHTML("beforeend", $memoryCardC);
 
+
+  $root.insertAdjacentHTML("beforeend", $pointBar)
   $root.insertAdjacentElement("beforeend", $cardsWrapper);
 })();
