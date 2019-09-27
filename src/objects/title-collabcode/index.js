dd95235ddb1f93e330ea2b0667a5f42934a68129
Ray Zyphs <bruferviveiros@gmail.com>
2019-09-27 14:34:54 -0300

    Aula 40

---
+++ src/objects/title-collabcode/index.js
@@ -0,0 +1,28 @@
+const titleCollabcode = (function () {
+  const module = {};
+
+  module._style = () => {
+    const $head = document.querySelector("head");
+    const $style = document.createElement("style");
+
+    $style.textContent = `
+      .title-collabcode {
+        color: #F25A70;
+        font-size: 24px;
+        letter-spacing: 0.6px;
+        text-transform: uppercase;
+      }
+    `;
+
+    $head.insertAdjacentElement("beforeend", $style);
+  }
+
+  module.render = content => {
+    module._style();
+    return `<h1 class="title-collabcode">${content}</h1>`;
+  }
+
+  return {
+    render: module.render,
+  }
+})();
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -4,6 +4,12 @@
   const $loginButton = flatButton.render('Log in');
   const $signupButton = flatButton.render('Sign up', true);
 
-  $root.insertAdjacentHTML("beforeend", $loginButton)
-  $root.insertAdjacentHTML("beforeend", $signupButton)  
+  const $logoCollabcode = logoCollabcode.render();
+  const $titleCollabcode = titleCollabcode.render("Welcome!");
+  
+  const $logoWrapper = logoWrapper.render($logoCollabcode, $titleCollabcode);
+
+  $root.insertAdjacentHTML("beforeend", $loginButton);
+  $root.insertAdjacentHTML("beforeend", $signupButton);
+  $root.insertAdjacentHTML("beforeend", $logoWrapper);
 })();
