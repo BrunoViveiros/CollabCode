4125ccbd009988e46ebd6ff16761025f4585d2c2
Ray Zyphs <bruferviveiros@gmail.com>
2019-10-07 14:01:18 -0300

    Pegando o evento de click

--- src/objects/eye-collabcode/index.js
+++ src/objects/eye-collabcode/index.js
@@ -8,23 +8,31 @@ const eyeCollabcode = (function() {
     $style.textContent = `
       .eye-collabcode {
         display: block;
-        text-indent: -9999px;
         background: url(./img/eye.svg) no-repeat center;
+        text-indent: -9999px;
         width: 24px;
         height: 15px;
-
+        cursor: pointer;
       }
     `;
 
     $head.insertAdjacentElement("beforeend", $style);
   };
 
+  module.handleClick = () => {
+    console.log("click");
+  };
+
   module.render = () => {
     module._style();
-    return `<label class="eye-collabcode">Mostrar senha</label>`;
+    return `<label
+              class="eye-collabcode"
+              onClick="eyeCollabcode.handleClick()"  
+            >Mostrar senha</label>`;
   };
 
   return {
-    render: module.render
+    render: module.render,
+    handleClick: module.handleClick
   };
 })();
