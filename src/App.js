365d6e89f1b3da81d19cff6e475dacd360da526c
Ray Zyphs <bruferviveiros@gmail.com>
2019-10-24 11:54:40 -0300

    Criado o object Card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 
+import Card from "./objects/Card";
+
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Card />
     </>
   );
 }
---
+++ src/objects/Card/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./styles.css";
+
+const Card = () => <article className="card">Card</article>;
+
+export default Card;
