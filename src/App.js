d4cb13407027067d807c4ecbbde35ade05531ae1
Ray Zyphs <bruferviveiros@gmail.com>
2019-10-29 20:47:31 -0300

    Criado a variação light para o AboutLink

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => (
     <Checkbox id="show" value="show" content="Mostrar Eventos" />
 
     <About>
-      <AboutLink />
+      <AboutLink className="-light" />
     </About>
   </main>
 );
