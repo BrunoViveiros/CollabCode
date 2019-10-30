1a82f6a578b0e53cccc07a40049e6f1ed0145b33
Ray Zyphs <bruferviveiros@gmail.com>
2019-10-30 10:14:15 -0300

    Feito o CSS do AvatarProfile

--- src/objects/AvatarProfile/index.js
+++ src/objects/AvatarProfile/index.js
@@ -1,5 +1,8 @@
 import React from "react";
+import "./styles.css";
 
-const AvatarProfile = ({ src, alt }) => <img src={src} alt={alt} />;
+const AvatarProfile = ({ src, alt }) => (
+  <img className="avatar-profile" src={src} alt={alt} />
+);
 
 export default AvatarProfile;
