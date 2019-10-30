fae1c5a490b7ef3d6badf1a8b5bc2a7be02d43ba
Ray Zyphs <bruferviveiros@gmail.com>
2019-10-30 10:00:56 -0300

    Criado o Object AvatarProfile

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import "./App.css";
 
 import Checkbox from "./objects/Checkbox";
 import About from "./objects/About";
+import AvatarProfile from "./objects/AvatarProfile";
 
 import Header from "./components/Header";
 import Hashtag from "./components/Hashtag";
 import HeaderInternal from "./components/HeaderInternal";
+
+import Profile from "./img/avatar.jpg";
 
 const App = () => (
   <main className="app">
@@ -17,6 +20,8 @@ const App = () => (
 
     <About>
       <HeaderInternal />
+
+      <AvatarProfile src={Profile} alt="Avatar do Bruno Fernandes" />
     </About>
   </main>
 );
