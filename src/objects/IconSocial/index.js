15cc3b596265e6c0bc236cf582e4457a79e8a558
Ray Zyphs <bruferviveiros@gmail.com>
2019-10-30 11:01:20 -0300

    Feito a variação -active About

--- src/objects/IconSocial/index.js
+++ src/objects/IconSocial/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const IconSocial = ({ action, src, alt }) => (
-  <a href={action} target="_blank">
+  <a href={action} target="_blank" rel="noopener noreferrer">
     <img className="icon-social" src={src} alt={alt} />
   </a>
 );
