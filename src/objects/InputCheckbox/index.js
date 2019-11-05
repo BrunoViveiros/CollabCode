1ee8ef030873a0816a5822dbe9b8eea3baa5ea11
Ray Zyphs <bruferviveiros@gmail.com>
2019-11-05 12:00:46 -0200

    Refactoring do Label para LabelGame

--- src/objects/InputCheckbox/index.js
+++ src/objects/InputCheckbox/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./styles.css";
 
-import Label from "../Label";
+import LabelGame from "../LabelGame";
 import InputGame from "../InputGame";
 
 const InputCheckbox = ({ id = "", value = "", content = "" }) => (
   <>
     <InputGame id={id} type="input-checkbox" value={value} />
-    <Label htmlFor={id} content={content} />
+    <LabelGame htmlFor={id} content={content} />
   </>
 );
 export default InputCheckbox;
