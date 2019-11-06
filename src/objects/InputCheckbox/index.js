e5053b27b7414114b8ff57596960909a9d250704
Ray Zyphs <bruferviveiros@gmail.com>
2019-11-06 14:25:34 -0200

    Criado o evento de click no InputCheckbox e no LabelGame

--- src/objects/InputCheckbox/index.js
+++ src/objects/InputCheckbox/index.js
@@ -4,10 +4,14 @@ import "./styles.css";
 import LabelGame from "../LabelGame";
 import InputGame from "../InputGame";
 
+const handleClick = () => {
+  console.log("lala");
+};
+
 const InputCheckbox = ({ id = "", value = "", content = "" }) => (
   <>
     <InputGame id={id} type="checkbox" value={value} />
-    <LabelGame htmlFor={id} content={content} />
+    <LabelGame onClick={handleClick} htmlFor={id} content={content} />
   </>
 );
 export default InputCheckbox;
